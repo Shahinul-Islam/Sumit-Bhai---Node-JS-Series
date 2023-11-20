8f5ab2f1d890e2c7178a77a6094ae9e869737e9e
shahinul islam <shahinul37@outlook.com>
2023-11-20 08:51:33 +0600

    fs done

--- video05/index.js
+++ video05/index.js
@@ -1,4 +1,21 @@
 const path = require("path");
+const os = require("os");
+const fs = require("fs");
+
+// path
 const myPath = "C:/Node Js - Sumit Bhai/video05/index.js";
 const pathDetails = path.parse(myPath);
-console.log(pathDetails.root);
+// console.log(pathDetails.root);
+
+// os
+const platform = os.platform();
+const homeDir = os.homedir();
+const memory = os.freemem();
+const core = os.cpus();
+// console.log(core);
+
+// fs
+// fs.writeFileSync("newFile.txt", "hello node developer");
+fs.readFile("newFile.txt", (err, data) => {
+	console.log(data.toString());
+});
